f6152b4a97b58c8e228816314e437b9ec9b11bc5
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-03-05 12:17:54 +0300

    add undertow

--- src/main/java/ru/roketx/demo/CreditController.java
+++ src/main/java/ru/roketx/demo/CreditController.java
@@ -11,28 +11,29 @@ import ru.rocketx.generated.model.Credit;
 public class CreditController implements ru.rocketx.generated.internal.api.CreditApiIntApi,
         ru.rocketx.generated.internal.api.TestApi {
 
+
     @Override
     public Mono<ResponseEntity<Credit>> createCredit(Mono<Credit> credit, ServerWebExchange exchange) {
-        return Mono.empty(); // Возвращаем пустой Mono
+        return null;
     }
 
     @Override
     public Mono<ResponseEntity<Flux<Credit>>> getCredits(ServerWebExchange exchange) {
-        return Mono.empty(); // Возвращаем пустой Mono
+        return null;
     }
 
     @Override
     public Mono<ResponseEntity<Void>> deleteCredit(String id, ServerWebExchange exchange) {
-        return Mono.empty(); // Возвращаем пустой Mono
+        return null;
     }
 
     @Override
     public Mono<ResponseEntity<Credit>> getCreditById(String id, ServerWebExchange exchange) {
-        return Mono.empty(); // Возвращаем пустой Mono
+        return null;
     }
 
     @Override
     public Mono<ResponseEntity<Credit>> updateCredit(String id, Mono<Credit> credit, ServerWebExchange exchange) {
-        return Mono.empty(); // Возвращаем пустой Mono
+        return null;
     }
 }
